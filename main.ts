184e0b7e3c353ba50f19d914b668aa69a2ac4b6e
maimoona101 <am3999733@gmail.com>
2024-09-14 01:59:12 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,59 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollar
+
+
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt(
+    [
+        {
+            name: "pin",
+            message: "enter your pin",
+            type: "number"
+        }
+    ]
+);
+//     12345  ===  1234  -  false
+if (pinAnswer.pin === myPin) {
+    console.log("Correct pin code!!!"); 
+
+    let operationAns = await inquirer.prompt(
+        [
+            {
+                name:"operaton",
+                message:"please select option",
+                type:"list",
+                choices:["Withdraw", "check balance"]
+            }
+        ]
+    );
+
+    console.log(operationAns);
+    
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt(
+            [
+                {
+                    name: 'amount',
+                    message: "enter your amount",
+                    type: "number"
+                }   
+            ]
+        );
+        // =, -=, +=
+        myBalance -= amountAns.amount;
+        
+        console.log("Your remaining balance is: " + myBalance)
+    
+    } else if (operationAns.operation === "check balance"){
+        console.log("your balance is: " + myBalance)
+    }
+
+}   
+ 
+else {
+    console.log("Incorrect pin number");
+}
+
